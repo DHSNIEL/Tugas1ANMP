3a9706571a953725aa1194880a94b1be00d3b4a7
Kyrosta <georgiusm07@gmail.com>
2024-02-19 18:58:33 +0700

    new branch coba"

--- app/src/main/java/com/daniel/tugas1anmp/ui/login/LoginFragment.java
+++ app/src/main/java/com/daniel/tugas1anmp/ui/login/LoginFragment.java
@@ -1,4 +1,26 @@
 package com.daniel.tugas1anmp.ui.login;
 
 public class LoginFragment {
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        val notificationsViewModel =
+                ViewModelProvider(this).get(NotificationsViewModel::class.java)
+
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textNotifications
+        notificationsViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
